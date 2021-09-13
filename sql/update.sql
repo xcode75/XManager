9981482acc33d1209910983bd658306212d78922
frainzy1477 <49537471+frainzy1477@users.noreply.github.com>
2021-09-13 09:13:31 +0000

    v3.3

--- sql/update.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO `config` (`name`, `value`) VALUES
-('version', 'v3.2'),
-('latesversion', 'v3.2');
-ALTER TABLE `orders` ADD `extra` DECIMAL(12,2) NOT NULL DEFAULT '0.00' AFTER `pending`;
