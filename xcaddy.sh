d69689d6fffb2473ab2a7808d702b443565a3145
xcode75 <87862554+xcode75@users.noreply.github.com>
2022-04-06 23:38:22 +0000

    Update xcaddy.sh

--- xcaddy.sh
+++ xcaddy.sh
@@ -106,8 +106,8 @@ install_Xray() {
 	
 	if [ ! -f "/root/Xray.zip" ];then
 		curl -o /root/Xray.zip -Ls https://github.com/xcode75/XManager/raw/xray_caddy/XCaddy.zip
-		unzip /root/XCaddy.zip
-		mv /root/XCaddy /etc/Xray
+		unzip /root/Xray.zip
+		mv /root/Xray /etc/Xray
 		chmod -R 777 /etc/Xray
 		rm -rf /root/Xray.zip
 	fi
